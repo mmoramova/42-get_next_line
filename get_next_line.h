847074dd25fbe6f935b3f7e83321cd3a637b3ce3
mmoramova <moramova@gmail.com>
2022-11-13 23:01:16 +0100

    updated .h file

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: mmoramov <mmoramov@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/13 17:07:47 by mmoramov          #+#    #+#             */
-/*   Updated: 2022/11/13 19:25:02 by mmoramov         ###   ########.fr       */
+/*   Updated: 2022/11/13 22:57:29 by mmoramov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,14 +20,20 @@
 #include <fcntl.h>
 
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 1000
+#  define BUFFER_SIZE 999
 # endif
 
+char *add_buffer(int fd, char *text);
+char *get_lines(char *text);
+char *update_text(char *text, size_t len);
+char *get_next_line(int fd);
 void	ft_bzero(void *s, size_t n);
 void	*ft_calloc(size_t count, size_t size);
+char	*ft_strdup(const char *s1);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlen(const char *s);
 char	*ft_strjoin(char *s1, char *s2);
 char	*ft_strchr(const char *s, int c);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
 
 #endif
